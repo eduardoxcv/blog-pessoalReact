b85b22674a0d629327ff1a072a7c7d5125fc5cb6
EduardoDev <97982807+eduardoxcv@users.noreply.github.com>
2022-03-22 00:25:34 -0300

    Adicionando a primeira atividade de React

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Home from "./paginas/home/Home";
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <Home />
+  );
+  
+}
+
+
+export default App;
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <h2><img src="https://i.pinimg.com/originals/48/07/e8/4807e883aa860a4df6e110d49be95cb5.jpg" alt="imagem velozes e furiosos" className="img"/></h2>
+
+        </>
+    );
+
+}
+
+export default Home;
